3a614fb09683a385fe454ceae515155c892c20fb
Hortense Chambron <hortense.chambron@gmail.com>
2024-05-03 16:22:21 +0200

    create function and refacto code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // menu burger
-document.getElementById('menu-burger').addEventListener('click', (event)=>{
+document.getElementById('menu-burger').addEventListener('click', (event) => {
     document.getElementById('burger-container').classList.toggle('menu-burger__container');
     document.getElementById('burger-container').classList.toggle('gradient');
     document.getElementById('menu-burger').classList.toggle('menu-burger__quit');
-    document.querySelectorAll('.js-menu-burger').forEach(function (jsBurger){
+    document.querySelectorAll('.js-menu-burger').forEach(function (jsBurger) {
         jsBurger.classList.toggle('menu-burger__itm');
     })
 })
@@ -12,45 +12,47 @@ document.getElementById('menu-burger').addEventListener('click', (event)=>{
 
 
 
-let json = [];
-fetch('../json/main-branch.json').then(response => {
-    return json = response.json();
-}).then(jsonStory => {
-    const storyTemplate = document.getElementById('storyTemplate');
-    const story = document.importNode(storyTemplate.content, true);
-    story.getElementById('history').textContent = jsonStory[0].story;
-   
-    story.getElementById('historyBtn1').textContent = jsonStory[0]['choice-1-txt'];
-    story.getElementById('historyBtn1').dataset.id = jsonStory[0]['choice-1-id'];
-    
-    story.getElementById('historyBtn2').textContent = jsonStory[0]['choice-2-txt'];
-    story.getElementById('historyBtn2').dataset.id = jsonStory[0]['choice-2-id'];
-    
-    story.getElementById('historyBtn3').textContent = jsonStory[0]['choice-3-txt'];
-    story.getElementById('historyBtn3').dataset.id = jsonStory[0]['choice-3-id'];
 
-    document.getElementById('playGround').appendChild(story)
+fetch('../json/main-branch.json').then(response => {
+    return response.json();
+}).then(jsonStory => {
+
+  letTheStoryGoesOn(jsonStory);
+  clickToUnrollStory();
 
 })
 
+//on click, get the json file and call the function to print it in the html
+function clickToUnrollStory(){
+    // On click, the story goes on 
+    document.getElementById('history-container').addEventListener('click', function (event) {
+        if (!event.target.classList.contains('history__btn')) return;
+        const choice = event.target.dataset.id;
+        fetch(`../json/branch-${choice}.json`).then(response => {
+            return response.json();
+        }).then(jsonStory => {
+            document.getElementById('playGroundContent').remove();
+            letTheStoryGoesOn (jsonStory);
+            clickToUnrollStory();
+        })
+    })
+}
 
 
 
-
-
-
-// document.getElementById('history-container').addEventListener('click', function (event){
-//     if(!event.target.classList.contains('history__btn')) return;
-//     document.getElementById('history').innerHTML = getRandomBinaryText();
-//     event.target.innerHTML = getRandomBinaryText();
-// })
-
-// function getRandomBinaryText(){
-//     const max = Math.floor(Math.random() * 100) + 1;
-//     let txt = '';
-//     for(let i = 0; i < max; i++){
-//         txt += Math.floor((Math.random() * 50) % 2).toString(36);
-//     }
-//     console.log(txt);
-//     return txt;
-// }
+/**
+ * Get the Json file and inject it in the html
+ * @param {array} jsonStory 
+ */
+function letTheStoryGoesOn (jsonStory){
+    const story = document.importNode(document.getElementById('storyTemplate').content, true);
+    story.getElementById('history').textContent = jsonStory[0].story;
+    
+    let i = 1;
+    jsonStory[0].choices.forEach(choice =>{
+        story.getElementById(`historyBtn${i}`).textContent = choice.txt;
+        story.getElementById(`historyBtn${i}`).dataset.id = choice.id;
+        i++;
+    })
+    document.getElementById('playGround').appendChild(story);
+}
