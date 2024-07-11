fc6c315e0d79f25a9aeed962db15efaabe42ee91
Hortense Chambron <hortense.chambron@gmail.com>
2024-07-11 12:22:47 +0200

    ajax is working

--- app/js/script.js
+++ app/js/script.js
@@ -10,27 +10,14 @@ document.getElementById('menu-burger').addEventListener('click', (event) => {
 
 
 
-
-
-
-fetch('../json/branch-0.json').then(response => {
-    return response.json();
-}).then(jsonStory => {
-  letTheStoryGoesOn(jsonStory);
-  clickToUnrollStory();
-})
-
-//on click, get the json file and call the function to print it in the html
+//on click, call the API and call the function to print it in the html
 function clickToUnrollStory(){
     // On click on a button, the story goes on 
     document.getElementById('history-container').addEventListener('click', function (event) {
         if (!event.target.classList.contains('history__btn')) return;
         const choice = event.target.dataset.id;
-        fetch(`../json/branch-${choice}.json`)
-        .then(response => {
-            return response.json();
-        })
-        .then(jsonStory => {
+        callAPI('POST', {action: 'choice', id: choice})
+                .then(jsonStory => {
             //remove the previous story...
             document.getElementById('playGroundContent').remove();
             //...and inject the next one
@@ -51,35 +38,42 @@ function clickToUnrollStory(){
 function letTheStoryGoesOn (jsonStory){
     //inject story's text
     const story = document.importNode(document.getElementById('storyTemplate').content, true);
-    story.getElementById('history').textContent = jsonStory[0].story;
+    story.getElementById('history').textContent = jsonStory[0].text;
     document.getElementById('playGround').appendChild(story);
     //inject story's button
     const btnContainer = document.getElementById('history-container');
-    jsonStory[0].choices.forEach(choice =>{
+    jsonStory.forEach(choice =>{
         const btn = document.importNode(document.getElementById('buttonTemplate').content, true);
-        btn.querySelector('.js-btn').textContent = choice.txt;
+        btn.querySelector('.js-btn').textContent = choice.choice;
         btn.querySelector('.js-btn').dataset.id = choice.id;
         btnContainer.appendChild(btn);
     })
 }
 
-function saveObjectInLS(object){
-    if(object[0].object !== undefined){
-        localStorage.setItem(object[0].object, )
-    }
-}
+// function saveObjectInLS(object){
+//     if(object[0].object !== undefined){
+//         localStorage.setItem(object[0].object, )
+//     }
+// }
 
 
 
 async function callAPI(method, params) {
     try {
-        const response = await fetch("../php/api.php", {
-            method: method,
-            body: JSON.stringify(params),
-            headers: {
-                'Content-type': 'application/json'
-            }
-        });
+        let obj = {}
+        if(method != 'GET'){
+            obj = {
+                method: method,
+                body: JSON.stringify(params),
+                headers: {
+                    'Content-type': 'application/json'
+                }};
+            params = '?';
+        }
+        else{
+            params += constructRequest(params);
+        }
+        const response = await fetch(`../php/api.php${params}`, obj);
         const json = await response.json();
         return json
     }
@@ -88,4 +82,18 @@ async function callAPI(method, params) {
     }
 }
 
-callAPI('POST', {}).then(data => console.log(data));
+
+function constructRequest (params){
+    let request ='';
+    for(const value in params){
+        request += `${value}=${params[value]}&`;
+    }
+    //return the string whitout the last '&'
+    return request.slice(0, -1);
+}
+
+
+callAPI('POST', {action: 'choice', id: 0}).then(jsonStory => {
+  letTheStoryGoesOn(jsonStory);
+  clickToUnrollStory();
+})
--- app/php/api.php
+++ app/php/api.php
@@ -11,14 +11,13 @@ global $errors;
 
 $inputData = json_decode(file_get_contents('php://input'), true);
 
-$query = $dbCo->prepare('SELECT c.text AS choice, id_body_text_leads_to, b.text
-FROM choice c
-JOIN possess USING (id_choice)
-JOIN body_text b USING (id_body_text)
-WHERE id_body_text = 0;');
-$query->execute();
-$result = $query->fetchAll();
-echo json_encode($result);
+    $query = $dbCo->prepare('SELECT c.text AS choice, id_body_text_leads_to AS id, b.text
+    FROM choice c
+    JOIN possess USING (id_choice)
+    JOIN body_text b USING (id_body_text)
+    WHERE id_body_text = :id;');
+    $query->execute(['id' => intval($inputData['id'])]);
+    $result = $query->fetchAll();
+    echo json_encode($result);
 
-// fetchall / fetchcolumn
 
